60112edeea6a201b7b6b020f4099c2f319f7e4b4
TheKevSter-NL <43183768+TheKevSter-NL@users.noreply.github.com>
2020-02-03 20:42:05 +0100

    Adding more search info

--- script.js
+++ script.js
@@ -1,60 +1,43 @@
-const app = document.getElementById('root')
+const app = document.getElementById('list')
 
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
+const block = document.createElement('li')
+block.setAttribute('class', 'results')
 
-app.appendChild(container)
+app.appendChild(block)
 
 var request = new XMLHttpRequest()
-// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-
-// request.onload = function() {
-//   // Begin accessing JSON data here
-//   var data = JSON.parse(this.response)
-//   if (request.status >= 200 && request.status < 400) {
-//     data.forEach(movie => {
-//       const card = document.createElement('div')
-//       card.setAttribute('class', 'card')
-
-//       const title = document.createElement('title')
-//       title.textContent = movie.title
-
-//       const p = document.createElement('p')
-//       movie.description = movie.description.substring(0, 300)
-//       p.textContent = `${movie.description}...`
-
-//       container.appendChild(card)
-//       card.appendChild(title)
-//       card.appendChild(p)
-//     })
-//   } else {
-//     const errorMessage = document.createElement('marquee')
-//     errorMessage.textContent = `Gah, it's not working!`
-//     app.appendChild(errorMessage)
-//   }
-// }
-request.open('GET', 'https://orion.apiseeds.com/api/music/lyric/Breaking Benjamin/Dear agony?apikey=3GIKwZk8GNseBxT3CZsEHLG3Ee34x2KEzjkqyHjzJ9fnvfbpqbH7Kx7zi5QkSuE7 ', true)
-
+// request.open('GET', 'https://orion.apiseeds.com/api/music/lyric/Breaking Benjamin/Dear agony?apikey=3GIKwZk8GNseBxT3CZsEHLG3Ee34x2KEzjkqyHjzJ9fnvfbpqbH7Kx7zi5QkSuE7 ', true)
+request.open('GET', 'https://orion.apiseeds.com/api/music/search/?q=Killpop&apikey=3GIKwZk8GNseBxT3CZsEHLG3Ee34x2KEzjkqyHjzJ9fnvfbpqbH7Kx7zi5QkSuE7&limit=20 ', true)
 request.onload = function() {
     // Begin accessing JSON data here
     var data = JSON.parse(this.response)
     if (request.status >= 200 && request.status < 400) {
         console.log(data.result)
-        const card = document.createElement('div')
-        card.setAttribute('class', 'card')
+
+        data.result.forEach( list => {
+        const card = document.createElement('ul')
+       
     
-        const title = document.createElement('h2')
-        title.textContent = data.result.artist.name
+        const artist = document.createElement('h3')
+        artist.textContent = ("Artist: ") + list.artist
+        
+        const title = document.createElement('p')
+        title.textContent = ("Song: ") + list.title
+
+      
+        const cover = document.createElement('img')
+        cover.src = list.cover
+        const album = document.createElement('P')
+        album.textContent = ("Album: ") + list.album
     
-        const lyrics = document.createElement('p')
-        data.result.track.text = data.result.track.text
-        lyrics.textContent = `${data.result.track.text}...`
-    
-        container.appendChild(card)
+        block.appendChild(card)
+        card.appendChild(artist)
         card.appendChild(title)
-        card.appendChild(lyrics)
+        card.appendChild(cover)
+        card.appendChild(album)
+        })
     } else {
-      const errorMessage = document.createElement('marquee')
+      const errorMessage = document.createElement('P')
       errorMessage.textContent = `Gah, it's not working!`
       app.appendChild(errorMessage)
     }
