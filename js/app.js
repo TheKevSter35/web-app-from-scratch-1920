737a2b016077e049095c2a1f9e688ab245da9fba
TheKevSter-NL <43183768+TheKevSter-NL@users.noreply.github.com>
2020-03-09 19:47:18 +0100

    added search function / default search

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
-import { api} from './modules/api.js';
+import { api, apiUserinput} from './modules/api.js';
+
+const button = document.getElementById('submitbtn')
+button.addEventListener("click", apiUserinput);
+
 
 api()
--- js/modules/api.js
+++ js/modules/api.js
@@ -3,8 +3,8 @@ import { fetchdata } from './fetchdata.js';
 export function api (){
   let endpoint = 'http://api.giphy.com/v1/gifs/search?'
   let key = "&api_key=" + "quS2dai4NJv0dJVwt1KN0r1GxnGHx6B4"
-  let searchword = "&q=" + "cats"
-  let limit = "&limit=" + "24"
+  let searchword = "&q=" + "nerd"
+  let limit = "&limit=" + "20"
   let offset = "&offset=" + "0"
   let rating = "&rating=" + "g"
   let lang = "&lang=" + "en"
@@ -12,3 +12,25 @@ export function api (){
 
   fetchdata(url)
 }
+const button = document.getElementById('submitbtn')
+button.addEventListener("click", apiUserinput);
+
+export function apiUserinput (){
+
+  const button = document.getElementById('submitbtn')
+button.addEventListener("click", apiUserinput);
+  let userInput = document.getElementById("user-input-field").value
+    console.log("Searching for: ", userInput)
+
+  let endpoint = 'http://api.giphy.com/v1/gifs/search?'
+  let key = "&api_key=" + "quS2dai4NJv0dJVwt1KN0r1GxnGHx6B4"
+  let searchword = "&q=" + (userInput)
+  let limit = "&limit=" + "20"
+  let offset = "&offset=" + "0"
+  let rating = "&rating=" + "g"
+  let lang = "&lang=" + "en"
+  let url = endpoint + key + searchword + limit + offset + rating;
+
+  fetchdata(url)
+}
+
